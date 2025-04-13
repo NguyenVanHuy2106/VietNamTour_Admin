0d8af9054e1203e6f154b2c127106c5cb9d0049e
Nguyễn Văn Huy <77230707+NguyenVanHuy2106@users.noreply.github.com>
2025-04-13 12:09:13 +0700

    xoá

--- src/app/GuideTravel/index.js
+++ src/app/GuideTravel/index.js
@@ -1,12 +1,10 @@
 // src/pages/Users.js
 import React from "react";
 
-import Word from "../../components/Word";
-
 const GuideTravel = () => {
   return (
     <div className="d-flex" style={{ marginLeft: "50px", marginRight: "50px" }}>
-      <Word />
+      Huy
     </div>
   );
 };
